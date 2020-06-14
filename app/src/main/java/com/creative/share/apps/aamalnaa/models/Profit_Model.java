a0dabeb59e53affa77573f195aae48e698798c62
End Point <ahmedmohamed23345@gmail.com>
2020-06-14 14:39:42 +0200

    ch

--- app/src/main/java/com/creative/share/apps/aamalnaa/models/Profit_Model.java
+++ app/src/main/java/com/creative/share/apps/aamalnaa/models/Profit_Model.java
@@ -11,34 +11,34 @@ public class Profit_Model implements Serializable {
 
     public static class Data implements Serializable {
         private int id;
-               private double ads_price;
-          private double pre_price;
-          private double rate_price;
-          private double total_price;
-                private int user_id;
+        private double ads_price;
+        private double pre_price;
+        private double rate_price;
+        private double total_price;
+        private int user_id;
 
-          public int getId() {
-              return id;
-          }
+        public int getId() {
+            return id;
+        }
 
-          public double getAds_price() {
-              return ads_price;
-          }
+        public double getAds_price() {
+            return ads_price;
+        }
 
-          public double getPre_price() {
-              return pre_price;
-          }
+        public double getPre_price() {
+            return pre_price;
+        }
 
-          public double getRate_price() {
-              return rate_price;
-          }
+        public double getRate_price() {
+            return rate_price;
+        }
 
-          public double getTotal_price() {
-              return total_price;
-          }
+        public double getTotal_price() {
+            return total_price;
+        }
 
-          public int getUser_id() {
-              return user_id;
-          }
-      }
+        public int getUser_id() {
+            return user_id;
+        }
+    }
 }
